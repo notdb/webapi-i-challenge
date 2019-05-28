9a896fc33043f9b3f365180e48d16efe6185004e
Doug <me@douglasbowen.com>
2019-05-27 22:04:35 -0400

    add get all users and get user by id

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+
+const express = require("express");
+const dbAccess = require("./data/db.js");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+server.get("/api/users", (req, res) => {
+  dbAccess
+    .find()
+    .then(users => res.send(users))
+    .catch(err => res.status(500).send(err));
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const userID = req.params.id;
+  dbAccess
+    .findById(userID)
+    .then(user => res.json(user))
+    .catch(err => res.status(500).send(err));
+});
+
+server.listen(8000, () => console.log("API running on port 8000"));
