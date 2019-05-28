26ce8d011727e65cc87baf853c1625d2bf528b83
Doug <me@douglasbowen.com>
2019-05-27 22:32:16 -0400

    add rest of the endpoints

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dbAccess = require("./data/db.js");
 
 const server = express();
 
+server.use(express.json());
+
 server.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -21,6 +23,39 @@ server.get("/api/users/:id", (req, res) => {
   dbAccess
     .findById(userID)
     .then(user => res.json(user))
+    .catch(err => res.status(500).send(err));
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const userID = req.params.id;
+  dbAccess
+    .remove(userID)
+    .then(user => res.json(user) && res.send(`${userID} removed`))
+    .catch(err => res.status(500).send(err));
+});
+
+server.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  dbAccess
+    .insert(newUser)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const userBody = req.body;
+  dbAccess
+    .update(id, userBody)
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ err: "incorrect id" });
+      }
+    })
     .catch(err => res.status(500).send(err));
 });
 
